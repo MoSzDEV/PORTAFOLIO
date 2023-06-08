61b9ed095d1844d0002f5887b27b4a69b4449559
MARIANOSDEV <nmkilmes@gmail.com>
2023-06-08 11:00:51 -0300

    submit 1.2.2

--- contact.js
+++ contact.js
@@ -1,10 +1,5 @@
 let form = document.getElementById("form")
-<<<<<<< HEAD
-
 form.action = 'https://formsubmit.co/31ac8cd7b28df87b7d952999d606171a';
-=======
-form.action = 'https://formsubmit.co/moszdev@gmail.com';
->>>>>>> 364ad57df8010e061399725c17c169905d601f11
 form.method = 'post';
 
 form.onsubmit = () => {
